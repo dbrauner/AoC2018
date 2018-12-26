3ea4a26af1628b5622deb4835a68fa7cff9d0433
Douglas <dbbrauner@gmail.com>
2018-12-26 20:19:50 -0200

    fake tests

--- day15/Answer.py
+++ day15/Answer.py
@@ -2,6 +2,129 @@ import operator
 import os
 import sys
 
+import heapq
+import operator
+
+# from . import custom_graph
+
+
+# from .custom_graph import *
+# import CustomGraph
+
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
+
+    def empty(self):
+        return len(self.elements) == 0
+
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
+
+    def get(self):
+        return heapq.heappop(self.elements)[1]
+
+
+#
+# class SimpleGraph:
+#     def __init__(self):
+#         self.edges = {}
+#
+#     def neighbors(self, id):
+#         return self.edges[id]
+#
+
+class SquareGrid:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.obstacles = []
+
+    def in_bounds(self, id):
+        (x, y) = id
+        return 0 <= x < self.width and 0 <= y < self.height
+
+    def passable(self, id):
+        return id not in self.obstacles
+
+    def neighbors(self, id):
+        (x, y) = id
+        results = [(x + 1, y), (x, y - 1), (x - 1, y), (x, y + 1)]
+
+        results.sort(key=operator.itemgetter(0, 1))
+
+        # if (x + y) % 2 == 0: results.reverse()  # aesthetics
+        results = filter(self.in_bounds, results)
+        results = filter(self.passable, results)
+        return results
+
+
+class GridWithWeights(SquareGrid):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.weights = {}
+
+    def cost(self, from_node, to_node):
+        return self.weights.get(to_node, 1)
+
+def heuristic(a, b):
+    (x1, y1) = a
+    (x2, y2) = b
+    return abs(x1 - x2) + abs(y1 - y2)
+
+
+def a_star_search(graph, start, goal):
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    came_from = {}
+    cost_so_far = {}
+    came_from[start] = None
+    cost_so_far[start] = 0
+
+    while not frontier.empty():
+        current = frontier.get()
+
+        if current == goal:
+            break
+
+        for next in graph.neighbors(current):
+            new_cost = cost_so_far[current] + graph.cost(current, next)
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost + heuristic(goal, next)
+                frontier.put(next, priority)
+                came_from[next] = current
+
+    return came_from, cost_so_far
+
+def reconstruct_path(came_from, start, goal):
+    current = goal
+    path = []
+    while current != start:
+        path.append(current)
+        current = came_from[current]
+    path.append(start) # optional
+    path.reverse() # optional
+    return path
+
+#
+# def neighbors(node, _map, d_units):
+#     dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+#     result = []
+#     for dir in dirs:
+#         neighbor = [node[0] + dir[0], node[1] + dir[1]]
+#         if 0 <= neighbor[0] < len(_map) and 0 <= neighbor[1] < len(_map[0]) and neighbor not in d_units:
+#             result.append(neighbor)
+#     return result
+
+def create_graph_from_map(_map, d_units, unit):
+    grid = GridWithWeights(len(_map[0]), len(_map))
+    for i, x in enumerate(_map):
+        for j, y in enumerate(x):
+            if _map[i][j] == '#' or (i, j) in d_units:
+                if (i, j) != (unit._x, unit._y): # ignores the unit itself
+                    grid.obstacles.append((i, j))
+    return grid
 
 class Unit:
     def __init__(self, _class, _x, _y):
@@ -64,32 +187,43 @@ def steps_to_reach(x, y, el, _map, units, visited, steps):
 
     visited.append((x, y))
 
-    next = steps_to_reach(x - 1, y, el, _map, units, visited, steps)
-    if next > -1:
-        # steps = next
-        # if next > steps:
-        return next
-    next = steps_to_reach(x, y - 1, el, _map, units, visited, steps)
-    if next > -1:
-        steps = next
-        # if next > steps:
-        return next
-    next = steps_to_reach(x, y + 1, el, _map, units, visited, steps)
-    if next > -1:
-        steps = next
-        # if next > steps:
-        return next
-    next = steps_to_reach(x + 1, y, el, _map, units, visited, steps)
-    if next > -1:
-        steps = next
-        # if next > steps:
-        return next
-    return -1
+    a = steps_to_reach(x - 1, y, el, _map, units, visited, steps)
+    # if x > - 1:
+    #     a = x
+    # if next > -1:
+    #     # steps = next
+    #     # if next > steps:
+    #     return next
+    b = steps_to_reach(x, y - 1, el, _map, units, visited, steps)
+    # if next > -1:
+    #     steps = next
+    #     # if next > steps:
+    #     return next
+    c = steps_to_reach(x, y + 1, el, _map, units, visited, steps)
+    # if next > -1:
+    #     steps = next
+    #     # if next > steps:
+    #     return next
+    d = steps_to_reach(x + 1, y, el, _map, units, visited, steps)
+    # if next > -1:
+    #     steps = next
+    #     # if next > steps:
+    #     return next
+    _range = [x for x in [a, b, c, d] if x > -1]
+    if len(_range) > 0:
+        return min(_range)
+    else:
+        return - 1
 
 
 def calculate_steps(unit, enemy, _map, units, move):
-    range = list()
     d_units = {(unit._x, unit._y): unit.life for unit in units}
+    graph = create_graph_from_map(_map, d_units)
+    came_from, cost_so_far = a_star_search(graph, (unit._x, unit._y), (enemy._x, enemy._y))
+
+    # path = reconstruct_path(came_from, (unit._x, unit._y), (enemy._x, enemy._y))
+    range = list()
+
     if check_position(enemy._x - 1, enemy._y, d_units, _map) or (enemy._x - 1, enemy._y) == (unit._x, unit._y):
         range.append((enemy._x - 1, enemy._y))
     if check_position(enemy._x, enemy._y - 1, d_units, _map) or (enemy._x, enemy._y - 1) == (unit._x, unit._y):
@@ -144,7 +278,35 @@ def attack_enemy(unit, enemy):
     # pass
 
 
-def do_turn(unit, units, _map):
+def calculate_enemy_cost(unit, enemy, _map, units):
+    d_units = {(unit._x, unit._y): unit.life for unit in units}
+    graph = create_graph_from_map(_map, d_units, unit)
+    came_from, cost_so_far = a_star_search(graph, (unit._x, unit._y), (enemy._x, enemy._y))
+
+    destinations = list(graph.neighbors((enemy._x, enemy._y)))
+    costs = [value for key, value in cost_so_far.items() if key in destinations]
+    if len(costs) == 0:
+        return -1
+    return min(costs)
+
+
+def move_to_enemy(unit, enemy, _map, units):
+    d_units = {(unit._x, unit._y): unit.life for unit in units}
+    graph = create_graph_from_map(_map, d_units, unit)
+    came_from, cost_so_far = a_star_search(graph, (unit._x, unit._y), (enemy._x, enemy._y))
+
+    destinations = list(graph.neighbors((enemy._x, enemy._y)))
+    costs = {key: value for key, value in cost_so_far.items() if key in destinations}
+    key_destination = next(iter(costs))
+    _next = key_destination
+    while came_from[_next] != (unit._x, unit._y):
+        _next = came_from[_next]
+
+    unit._x = _next[0]
+    unit._y = _next[1]
+
+
+def do_turn(unit, units, _map, counter):
     continue_battle = True
     enemies = find_enemies(unit, units)
     if len(enemies) == 0:
@@ -154,8 +316,14 @@ def do_turn(unit, units, _map):
     enemy_to_attack = - 1
     near_distance = sys.maxsize
     minimum_hp = sys.maxsize
+
+    # attack_queue = PriorityQueue()
     for i, enemy in enumerate(enemies):
-        steps = calculate_steps(unit, enemy, _map, units, False)
+
+        steps = calculate_enemy_cost(unit, enemy, _map, units)
+        # attack_queue.put(enemy, steps)
+
+        # steps = calculate_steps(unit, enemy, _map, units, False)
         if near_distance > steps > - 1:
             enemy_to_attack = i
             near_distance = steps
@@ -163,13 +331,17 @@ def do_turn(unit, units, _map):
         if near_distance == steps and enemy.life < minimum_hp:
             enemy_to_attack = i
             minimum_hp = enemy.life
-    if near_distance > 0:
-        calculate_steps(unit, enemies[enemy_to_attack], _map, units, True)
+
+    if sys.maxsize > near_distance > 0:
+        # calculate_steps(unit, enemies[enemy_to_attack], _map, units, True)
+        move_to_enemy(unit, enemies[enemy_to_attack], _map, units)
         near_distance -= 1
 
     if near_distance == 0:
         attack_enemy(unit, enemies[enemy_to_attack])
 
+    os.system('clear')
+    print('Round ' + str(counter))
     print_map(_map, units)
 
     return continue_battle
@@ -199,7 +371,7 @@ def do_it(_input):
         print_map(_map, units)
 
         for unit in units:
-            continue_battle = do_turn(unit, units, _map)
+            continue_battle = do_turn(unit, units, _map, counter)
             if not continue_battle:
                 break
         units = [x for x in units if x.life > 0]
@@ -217,10 +389,15 @@ def do_it(_input):
 if __name__ == '__main__':
     print('day 15')
     print('#Test Set')
-    # file = open("test_input.txt", 'r')
+
+    # file = open("test_input_custom.txt", 'r')
     # _test_input = file.read().split('\n')
     # assert do_it(_test_input) == 27730
     #
+    # file = open("test_input.txt", 'r')
+    # _test_input = file.read().split('\n')
+    # assert do_it(_test_input) == 27730
+    # #
     # file = open("test_input_2.txt", 'r')
     # _test_input = file.read().split('\n')
     # assert do_it(_test_input) == 36334
@@ -237,10 +414,9 @@ if __name__ == '__main__':
     # _test_input = file.read().split('\n')
     # assert do_it(_test_input) == 18740
     #
+    # #
     #
-
-
     file = open("input.txt", 'r')
     _input = file.read().split('\n')
     print("#Solutions")
-    print(do_it(_input))
+    assert do_it(_input) < 258940
